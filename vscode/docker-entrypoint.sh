cc5d64820e860bd95112d8cbea06743566ffc239
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-09-09 07:32:25 +0200

    Update docker-entrypoint.sh

--- vscode/docker-entrypoint.sh
+++ vscode/docker-entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 if [ $# -eq 0 ]
   then
-    /usr/local/bin/code-server --allow-http --no-auth --data-dir /data /code
+    /usr/local/bin/code-server --cert=/certs/localhost.crt --cert-key=/certs/localhost.key --data-dir /data /code
+    #/usr/local/bin/code-server --cert=~/certs/localhost.crt --cert-key=~/certs/localhost.key --allow-http --no-auth --data-dir /data /code
   else
     exec "$@"
 fi
