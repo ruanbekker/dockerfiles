e63bf1912238c6338c86845fba95d7f2bcdb1949
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-01-28 15:05:09 +0200

    Update build.sh

--- drone/agent/build.sh
+++ drone/agent/build.sh
@@ -5,6 +5,6 @@ wget https://github.com/drone/drone/archive/v${DRONE_BUILD_NUMBER}.zip
 unzip v$DRONE_BUILD_NUMBER.zip
 cd drone-$DRONE_BUILD_NUMBER/cmd/drone-agent/
 go get
-GOOS=linux GOARCH=amd64 go build -ldflags '-X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/linux/arm/drone-agent github.com/drone/drone/cmd/drone-agent
+GOOS=linux GOARCH=amd64 go build -ldflags '-X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/linux/amd64/drone-agent github.com/drone/drone/cmd/drone-agent
 mv release ../../../
 cd ../../..
