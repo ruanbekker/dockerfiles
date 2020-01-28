109137f02c3114de401ace2277d6b2b66f55c3ac
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-01-28 15:04:41 +0200

    Update build.sh

--- drone/server/build.sh
+++ drone/server/build.sh
@@ -5,6 +5,6 @@ wget https://github.com/drone/drone/archive/v${DRONE_BUILD_NUMBER}.zip
 unzip v$DRONE_BUILD_NUMBER.zip
 cd drone-$DRONE_BUILD_NUMBER/cmd/drone-server/
 go get
-GOOS=linux GOARCH=amd64 go build -ldflags '-X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/linux/arm/drone-server github.com/drone/drone/cmd/drone-server
+GOOS=linux GOARCH=amd64 go build -ldflags '-X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
 mv release ../../../
 cd ../../..
