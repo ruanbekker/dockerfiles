085a62c502e56a24402a3afb9c6653c04d3621b9
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-04-08 19:16:53 +0200

    Update app.js

--- nodejs-hostname/app.js
+++ nodejs-hostname/app.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var os = require('os');
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(`My Hostname: ${os.hostname()}\n`);
 }).listen(8080);
+
+console.log('Listening on :8080');
