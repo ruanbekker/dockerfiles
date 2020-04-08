f931b0d9d70b9ed08ea8bcd1421456172e98497c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-04-08 19:12:23 +0200

    Create app.js

---
+++ nodejs-hostname/app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var os = require('os');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`My Hostname: ${os.hostname()}\n`);
+}).listen(8080);
