0273ba96198ad89346397bb1e19a7887c5683074
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-11-10 15:41:28 +0200

    Create app.py

---
+++ aws-metadata-flask/app.py
@@ -0,0 +1,31 @@
+import socket
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def get_instance_details():
+    data = {}
+    instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').content
+    availability_zone = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone').content
+    instance_hostname = requests.get('http://169.254.169.254/latest/meta-data/hostname').content
+    container_hostname = socket.gethostname()
+
+    data['instance_id'] = instance_id.decode('utf-8')
+    data['availability_zone'] = availability_zone.decode('utf-8')
+    data['instance_hostname'] = instance_hostname.decode('utf-8')
+    data['container_hostname'] = container_hostname
+
+    return data
+
+@app.route('/')
+def get():
+    instance_details = get_instance_details()
+    return jsonify(instance_details)
+
+@app.route('/health')
+def health():
+    return jsonify({'message': 'ok'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
