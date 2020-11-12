39754aa169d95c0b1569fb1289449378c22b5f00
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-11-12 18:39:33 +0200

    Update app.py

--- aws-metadata-flask/app.py
+++ aws-metadata-flask/app.py
@@ -9,11 +9,13 @@ def get_instance_details():
     instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').content
     availability_zone = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone').content
     instance_hostname = requests.get('http://169.254.169.254/latest/meta-data/hostname').content
+    instance_lifecycle = requests.get('http://169.254.169.254/latest/meta-data/instance-life-cycle').content
     container_hostname = socket.gethostname()
 
     data['instance_id'] = instance_id.decode('utf-8')
     data['availability_zone'] = availability_zone.decode('utf-8')
-    data['instance_hostname'] = instance_hostname.decode('utf-8')
+    data['instance_hostname'] = instance_hostname.decode('utf-8').split('.')[0]
+    data['instance_lifecycle'] = instance_lifecycle.decode('utf-8')
     data['container_hostname'] = container_hostname
 
     return data
