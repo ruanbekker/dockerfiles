08fa8fb89a2b221bc52fe288b09c80be988b9150
Ruan Bekker <ruan.ru.bekker@gmail.com>
2021-09-15 22:37:01 +0200

    Update app.go

--- hostname-go/app.go
+++ hostname-go/app.go
@@ -3,17 +3,25 @@ package main
 import (
     "fmt"
     "os"
+    "log"
     "net/http"
 )
 
 func hostnameHandler(w http.ResponseWriter, r *http.Request) {
     myhostname, _ := os.Hostname()
+    log.Println("logging request for hostnameHandler on /")
     fmt.Fprintln(w, "Hostname:", myhostname)
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    log.Println("logging request for healthHandler on /health")
+    fmt.Fprintln(w, "ok")
+}
+
 func main() {
-    const port string = "8000"
-    fmt.Println("Server listening on port", port)
+    const port string = "8080"
+    log.Println("Server listening on port", port)
     http.HandleFunc("/", hostnameHandler)
+    http.HandleFunc("/health", healthHandler)
     http.ListenAndServe(":" + port, nil)
 }
